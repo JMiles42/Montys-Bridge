2468b8eda0849a29ba5d6125f9a47eca833e69db
JMiles42 <J@jmiles42.com>
2016-06-22 19:05:25 +1000

    CamRotate now Generic float value, not hard coded to input

--- Bridge Game/Assets/Scripts/Camera/CamMovement.cs
+++ Bridge Game/Assets/Scripts/Camera/CamMovement.cs
@@ -1,97 +1,35 @@
 using UnityEngine;
 using System.Collections;
 
-public class CamMovement : MonoBehaviour 
+public class CamMovement : MonoBehaviour
 {
-	public	float		rotateSpeed;//Rotate speed
-	public	float		zoomSpeed;//Zoom speed
-	public	Camera	cam;//Camera obj
-	public	int			curScreenWidth;//Screen width
-	public	int			curScreenHeight;//Screen hight
-	public	int			bounds;//Distance for mouse from edge of screen
-	public	int			zoomLevelLength;//How many zoom levels there are
-	public	Transform	zoom;//Zoom transform
-	public	Transform[]	zoomLevel;//All zoom level transforms
-	public	int			curZoomLevel;//Current zoom level
-	public	bool		canMove = false;//Is the mouse used to move
-	public	bool		isZoomIn= false;//Can zoom in
-	private float		mSpeed;//Move speed containter
-	private Vector3		myCamPos;//Camera pos
+	public  float       rotateSpeed;//Rotate speed
+	public  float       zoomSpeed;//Zoom speed
+	public  Camera		cam;//Camera obj
+	public  int         bounds;//Distance for mouse from edge of screen
+	private Vector3     myCamPos;//Camera pos
 	public  Animator    myZoomAnimator;
 
-	void Start()
-	{
-		//myCamPos = cam.transform.position;//Set defalt
-		curScreenHeight = Screen.height;//Screen Height Set
-		curScreenWidth = Screen.width;//Screen Width Set
-		zoomLevelLength = zoomLevel.Length;//Get zoom length
-	}
-
 	// Update is called once per frame
-	void Update () 
+	void Update()
 	{
-		//camZoom();//Call zoom
+		camZoom(Input.GetAxis("Mouse ScrollWheel"));//Call zoom
+		camRotate(Input.GetAxis("Horizontal"));//Call rotate
+	}
 
+	void camZoom(float amount)
+	{
+		//Set Zoom level for camera
 		myZoomAnimator.SetFloat
-			(
-				"ZoomLevel",
-				Mathf.Clamp
-				(
-					myZoomAnimator.GetFloat("ZoomLevel") +
-					(Input.GetAxis("Mouse ScrollWheel")),0,1
-				)
-			);
-
-		camRotate();//Call rotate
-		//
-		//cam.transform.LookAt(this.transform.position);//Camera look at controller
-		//cam.transform.position = zoom.position;//Set cam location to cur zoom location
+		(
+			"ZoomLevel",
+			Mathf.Clamp(myZoomAnimator.GetFloat("ZoomLevel") + (amount), 0, 1)
+		);
 	}
 
-	void camZoom()
+	void camRotate(float amount)
 	{
-		float sW = Input.GetAxis("Mouse ScrollWheel");
-		//Transform min;
-		//Transform max;
-        if( sW < 0 )//Zoom out
-		{
-			if(curZoomLevel > zoomLevel.Length)
-			{
-				curZoomLevel = zoomLevel.Length-1;//Make sure array does not go out of bounds
-			}
-			else
-			{
-				curZoomLevel++;//Zoom plus
-			}
-
-
-		}
-		else if( sW > 0 )//Zoom in
-		{
-			if(curZoomLevel <= 0)
-			{
-				curZoomLevel = 0;//Make sure array does not go out of bounds
-			}
-			else
-			{
-				curZoomLevel--;//Zoom minus
-			}
-
-
-		}
-		
-		zoom.transform.position = zoomLevel[curZoomLevel].transform.position;//Move zoom level transform
-	}
-
-	void camRotate()
-	{
-		if(Input.GetKey(KeyCode.Q))
-		{
-			transform.RotateAround(transform.localPosition,transform.up,rotateSpeed*Time.smoothDeltaTime);//Rotate left around look at target
-		}
-		else if(Input.GetKey(KeyCode.E))
-		{
-			transform.RotateAround(transform.position,transform.up,-rotateSpeed*Time.smoothDeltaTime);//Rotate right around look at target
-		}
+		//Rotate Vector Zero
+		transform.RotateAround(transform.localPosition, transform.up, rotateSpeed * amount * Time.smoothDeltaTime);
 	}
 }
