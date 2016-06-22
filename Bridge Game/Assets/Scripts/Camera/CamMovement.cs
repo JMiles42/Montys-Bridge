daade4888e39597f56665dcce0b65d53d4a8dc7d
JMiles42 <J@jmiles42.com>
2016-06-22 18:51:19 +1000

    Camera Zoom Controled by animater now

--- Bridge Game/Assets/Scripts/Camera/CamMovement.cs
+++ Bridge Game/Assets/Scripts/Camera/CamMovement.cs
@@ -1,30 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CamMovement : MonoBehaviour 
 {
-	public float moveSpeed;//Move speed
-	public float fastMoveSpeed;//Fast move speed
-	public float rotateSpeed;//Rotate speed
-	public float zoomSpeed;//Zoom speed
-	public float moveVertical;//Value from input.getaxis
-	public float moveHorizontal;//Value from input.getaxis
-	public GameObject cam;//Camera obj
-	public int curScreenWidth;//Screen width
-	public int curScreenHeight;//Screen hight
-	public int bounds;//Distance for mouse from edge of screen
-	public int zoomLevelLength;//How many zoom levels there are
-	public Transform zoom;//Zoom transform
-	public Transform[] zoomLevel;//All zoom level transforms
-	public int curZoomLevel;//Current zoom level
-	public bool canMove = false;//Is the mouse used to move
-	public bool isZoomIn= false;//Can zoom in
-	private float mSpeed;//Move speed containter
-	private Vector3 myCamPos;//Camera pos
+	public	float		rotateSpeed;//Rotate speed
+	public	float		zoomSpeed;//Zoom speed
+	public	Camera	cam;//Camera obj
+	public	int			curScreenWidth;//Screen width
+	public	int			curScreenHeight;//Screen hight
+	public	int			bounds;//Distance for mouse from edge of screen
+	public	int			zoomLevelLength;//How many zoom levels there are
+	public	Transform	zoom;//Zoom transform
+	public	Transform[]	zoomLevel;//All zoom level transforms
+	public	int			curZoomLevel;//Current zoom level
+	public	bool		canMove = false;//Is the mouse used to move
+	public	bool		isZoomIn= false;//Can zoom in
+	private float		mSpeed;//Move speed containter
+	private Vector3		myCamPos;//Camera pos
+	public  Animator    myZoomAnimator;
 
 	void Start()
 	{
-		myCamPos = cam.transform.position;//Set defalt
+		//myCamPos = cam.transform.position;//Set defalt
 		curScreenHeight = Screen.height;//Screen Height Set
 		curScreenWidth = Screen.width;//Screen Width Set
 		zoomLevelLength = zoomLevel.Length;//Get zoom length
@@ -33,31 +30,43 @@ public class CamMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		camZoom();//Call zoom
+		//camZoom();//Call zoom
+
+		myZoomAnimator.SetFloat
+			(
+				"ZoomLevel",
+				Mathf.Clamp
+				(
+					myZoomAnimator.GetFloat("ZoomLevel") +
+					(Input.GetAxis("Mouse ScrollWheel")),0,1
+				)
+			);
+
 		camRotate();//Call rotate
-
-		if (Input.GetKey(KeyCode.LeftShift))
-			mSpeed = fastMoveSpeed;//Set fast sprinting
-		else
-			mSpeed = moveSpeed;//Normal speed
-
-		cam.transform.LookAt(this.transform.position);//Camera look at controller
-		cam.transform.position = zoom.position;//Set cam location to cur zoom location
+		//
+		//cam.transform.LookAt(this.transform.position);//Camera look at controller
+		//cam.transform.position = zoom.position;//Set cam location to cur zoom location
 	}
+
 	void camZoom()
 	{
-		if(Input.GetAxis("Mouse ScrollWheel") <0 && curZoomLevel < 6)//Zoom out
+		float sW = Input.GetAxis("Mouse ScrollWheel");
+		//Transform min;
+		//Transform max;
+        if( sW < 0 )//Zoom out
 		{
-			if(curZoomLevel >= 2)
+			if(curZoomLevel > zoomLevel.Length)
 			{
-				curZoomLevel = 2;//Make sure array does not go out of bounds
+				curZoomLevel = zoomLevel.Length-1;//Make sure array does not go out of bounds
 			}
 			else
 			{
 				curZoomLevel++;//Zoom plus
 			}
+
+
 		}
-		if(Input.GetAxis("Mouse ScrollWheel") >0 && curZoomLevel > 0)//Zoom in
+		else if( sW > 0 )//Zoom in
 		{
 			if(curZoomLevel <= 0)
 			{
@@ -67,9 +76,13 @@ public class CamMovement : MonoBehaviour
 			{
 				curZoomLevel--;//Zoom minus
 			}
+
+
 		}
+		
 		zoom.transform.position = zoomLevel[curZoomLevel].transform.position;//Move zoom level transform
 	}
+
 	void camRotate()
 	{
 		if(Input.GetKey(KeyCode.Q))
