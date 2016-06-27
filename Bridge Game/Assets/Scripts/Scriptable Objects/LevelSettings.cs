50fe0757c44e543b75791f77a7a385899507f344
JMiles42 <J@jmiles42.com>
2016-06-27 23:51:18 +1000

    Level Settings Variables

--- Bridge Game/Assets/Scripts/Scriptable Objects/LevelSettings.cs
+++ Bridge Game/Assets/Scripts/Scriptable Objects/LevelSettings.cs
@@ -5,7 +5,15 @@ using System.Collections;
 //using System.Collections.Generic;
 //using JMiles42;
 
+[CreateAssetMenu(fileName = "New Level", menuName = "Monty's/New Level", order = 1)]
 public class LevelSettings : ScriptableObject
 {
-	
+	public string LevelName;
+	public VehicleSpawn[] vehicleSpawn;
+}
+[System.Serializable]
+public struct VehicleSpawn
+{
+	public Vehicle vehicle;
+	public int vehicleCount;
 }
