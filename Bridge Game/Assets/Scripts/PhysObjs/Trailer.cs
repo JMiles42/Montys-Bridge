0ce8639721ab31c809200b13b404ec5d2f9a536b
Jordan Miles <J@jmiles42.com>
2016-08-05 09:38:20 +1000

    Added truck with barrels

--- Bridge Game/Assets/Scripts/PhysObjs/Trailer.cs
+++ Bridge Game/Assets/Scripts/PhysObjs/Trailer.cs
@@ -3,5 +3,15 @@ using System.Collections;
 
 public class Trailer : PhysicalObject
 {
-
+	public float force = 3000;
+	[ContextMenu("forceMode")]
+	void SetJoimts()
+	{
+		FixedJoint[] fJ = GetComponents<FixedJoint> ();
+		foreach (FixedJoint f in fJ) 
+		{
+			f.breakForce = force;	
+			f.breakTorque = force;	
+		}
+	}
 }
