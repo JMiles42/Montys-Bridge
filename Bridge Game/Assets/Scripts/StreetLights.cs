bd3a324b3ff33444d753119695b8fe5b25781ce5
JMiles42 <J@jmiles42.com>
2016-08-09 15:53:48 +1000

    Added Audio & Police

---
+++ Bridge Game/Assets/Scripts/StreetLights.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+using System.Collections;
+//using System.Collections.Generic;
+//using JMiles42;
+
+public class StreetLights : MonoBehaviour 
+{
+	public Light light;
+	#region Events
+	protected void OnEnable()
+	{
+		EventManager.StartListening(EventStrings.LIGHTSOFF, LightsOff);
+		EventManager.StartListening(EventStrings.LIGHTSON, LightsOn);
+	}
+	protected void OnDisable()
+	{
+		EventManager.StopListening(EventStrings.LIGHTSOFF, LightsOff);
+		EventManager.StopListening(EventStrings.LIGHTSON, LightsOn);
+	}
+	#endregion
+
+	void LightsOn()
+	{
+		light.gameObject.SetActive(true);
+	}
+	void LightsOff()
+	{
+		light.gameObject.SetActive(false);
+	}
+}
