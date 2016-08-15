9f2195a2740d060fef40bb7e2ef5a9b1056f05e1
JMiles42 <J@jmiles42.com>
2016-08-15 12:25:39 +1000

    Changed map 9

--- Bridge Game/Assets/Scripts/Placing/TrapPlacer.cs
+++ Bridge Game/Assets/Scripts/Placing/TrapPlacer.cs
@@ -61,9 +61,9 @@ public class TrapPlacer : Singleton<TrapPlacer>
 	public void StartPlacingTrap()
 	{
 		StopTraps();
-			GridMaster.Instance.ShowGrid();
-			userPlacingTrap = true;
-			StartCoroutine(PlacingTrap());
+		GridMaster.Instance.ShowGrid();
+		userPlacingTrap = true;
+		StartCoroutine(PlacingTrap());
 	}
 	#region PlaceDownTrap
 	[ContextMenu("PlaceTraping")]
